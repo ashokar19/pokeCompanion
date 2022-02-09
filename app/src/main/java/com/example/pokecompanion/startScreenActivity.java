895e406dbad7083803f8817c251938ecce821a5e
a_shokar <arjun.shokar@gmail.com>
2022-02-09 12:24:30 -0800

    added music to start screen

--- app/src/main/java/com/example/pokecompanion/startScreenActivity.java
+++ app/src/main/java/com/example/pokecompanion/startScreenActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,8 @@ public class startScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
         setupAllButtons();
+        MediaPlayer music = MediaPlayer.create(startScreenActivity.this, R.raw.pokemon_center_music);
+        music.start();
     }
 
     public void setupAllButtons(){
