c25352165b837306065aeaf444910d60cc4e732b
a_shokar <arjun.shokar@gmail.com>
2022-02-13 20:29:18 -0800

    made add pokemon screen begin to add pokemon but bug where it doesn't find target text files occurs

--- app/src/main/java/com/example/pokecompanion/addPokemonActivity.java
+++ app/src/main/java/com/example/pokecompanion/addPokemonActivity.java
@@ -3,10 +3,28 @@ package com.example.pokecompanion;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.pokecompanion.models.pokemonObject;
+import com.example.pokecompanion.reader.readingDex;
+
+import java.io.IOException;
 
 public class addPokemonActivity extends AppCompatActivity {
+
+    readingDex randomReader;
+    pokemonObject pokemonToAdd;
+
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, addPokemonActivity.class);
@@ -16,7 +34,67 @@ public class addPokemonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_pokemon_screen);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        Button randomize = (Button) findViewById(R.id.randomizerBtn);
+        randomize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                   pokemonToAdd = readingDex.reader();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                TextView view = (TextView) findViewById(R.id.addedPokeNameLine);
+                try {
+                    view.setText(readingDex.reader().getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                finish();
+
+                Log.i("MyApp", "Task has been completed");
+            }
+        });
+
+
+//        Button checkTaskComplete = (Button) findViewById(R.id.taskCompleteCheck);
+//        checkTaskComplete.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//                editedTask.nextAssignee();
+//                Log.i("MyApp", "Task has been completed");
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_pokemon_tool,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_save_poke:
+
+                finish();
+                return true;
+
+            case android.R.id.home:
+                Toast.makeText(this, "Make sure you saved your pokemon choice!", Toast.LENGTH_SHORT).show();
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
--- app/src/main/java/com/example/pokecompanion/reader/readingDex.java
+++ app/src/main/java/com/example/pokecompanion/reader/readingDex.java
@@ -1,5 +1,6 @@
 package com.example.pokecompanion.reader;
 
+import com.example.pokecompanion.R;
 import com.example.pokecompanion.models.pokemonObject;
 
 import java.io.BufferedReader;
@@ -16,16 +17,14 @@ public class readingDex {
     private static String pokemonPick;
     private static String fileName;
     private static String[] pokeInfo;
-    private pokemonObject pokemon;
+    private static pokemonObject pokemon;
     static List<String> pokemonList = new ArrayList<>();
 
 
+    public static pokemonObject reader() throws IOException {
+        fileName = "test_dex";
 
-
-    public static void reader() throws IOException {
-        fileName = "testDex";
-
-        FileInputStream fstream = new FileInputStream(fileName);
+        FileInputStream fstream = new FileInputStream(String.valueOf(R.raw.test_dex));
         BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
         StringBuilder all = new StringBuilder();
         String fileLine;
@@ -33,6 +32,7 @@ public class readingDex {
         //readline reads the individual lines of the file using the buffered reader
 
         while ((fileLine = br.readLine()) != null) {
+            System.out.print(fileLine);
             pokemonList.add(fileLine);
             br.close();
         }
@@ -41,13 +41,21 @@ public class readingDex {
         Random rand = new Random();
         int randomPick = rand.nextInt(pokemonList.size());
         String selectedPick = pokemonList.get(randomPick);
+
         pokemonPick = selectedPick;
         System.out.println(pokemonPick);
+
         pokeInfo = pokemonPick.split(" ");
         int pokeID = Integer.parseInt(pokeInfo[0]);
-        String pokemonName = pokeInfo[1];
+        String pokeName = pokeInfo[1];
+        String pokeType = pokeInfo[2];
+        if (pokeInfo.length > 3) {
+            pokeType += " " + pokeInfo[3];
+        }
 
-        //make random pick here
+        pokemon = new pokemonObject(pokeName, pokeType, pokeID);
+
+        return pokemon;
 
     }
 
